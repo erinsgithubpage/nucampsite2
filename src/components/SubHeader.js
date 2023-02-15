e480b97fdc888102cda617abf12acae60fe5e0d6
erinh <erin.hazelwood@outlook.com>
2023-02-15 10:13:44 -0500

    More RR, four - 3:13

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -2,7 +2,18 @@ import { Col, Row, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 const SubHeader = ({ current, detail }) => {
-
+    return (
+        <Row>
+            <Col>
+                <Breadcrumb>
+                    <BreadcrumbItem>
+                        <Link to='/'>Home</Link>
+                    </BreadcrumbItem>
+                    
+                </Breadcrumb>
+            </Col>
+        </Row>
+    );
 };
 
 export default SubHeader;
