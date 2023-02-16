16e399c94ed53e175f6a8c1777e85b12b443d504
erinh <erin.hazelwood@outlook.com>
2023-02-16 06:25:08 -0500

    Workshop two, task one, step nine

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,16 @@
 import { Col, Row, Container, Card, CardBody, CardHeader } from 'reactstrap';
 
 const AboutPage = () => {
+    return (
+        <Container>
+            <Row className='row-content'>
 
+            </Row>
+            <Row className='row-content'>
+
+            </Row>
+        </Container>
+    )
 }
 
 export default AboutPage;
