e8b88658cbaf6688e3c2d93d28aab3fbeae07d26
erinh <erin.hazelwood@outlook.com>
2023-02-24 06:23:08 -0500

    Workshop three, task one completed

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,18 +1,28 @@
 import { Col } from 'reactstrap';
 import Comment from './Comment';
 import { selectCommentsByCampsiteId } from './commentsSlice';
+import CommentForm from './CommentForm';
 
 const CommentsList = ({ campsiteId }) => {
     const comments = selectCommentsByCampsiteId(campsiteId);
 
     if (comments && comments.length > 0) {
         return (
+            <>
+           
             <Col md='5' className='m-1'>
                 <h4>Comments</h4>
                 {comments.map((comment) => {
-                    return <Comment key={comment.id} comment={comment} />
-                })}
-            </Col> 
+                    return (
+                        <>
+                    
+                    <Comment key={comment.id} comment={comment} />
+                    
+                </>
+                )})}
+                <CommentForm campsiteId={campsiteId}/>
+            </Col>
+            </> 
         );
     }
     return (
