37fd4656d9a7f1a8d5fb01edc93d4e7090af209e
erinh <erin.hazelwood@outlook.com>
2023-02-25 11:02:23 -0500

    Workshop three, task two completed

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -8,21 +8,21 @@ const CommentsList = ({ campsiteId }) => {
 
     if (comments && comments.length > 0) {
         return (
-            <>
+          
            
             <Col md='5' className='m-1'>
                 <h4>Comments</h4>
                 {comments.map((comment) => {
                     return (
-                        <>
+                      
                     
                     <Comment key={comment.id} comment={comment} />
                     
-                </>
+               
                 )})}
                 <CommentForm campsiteId={campsiteId}/>
             </Col>
-            </> 
+         
         );
     }
     return (
