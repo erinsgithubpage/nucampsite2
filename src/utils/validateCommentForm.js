414a76b797a423a69a245831d8ebcfabd127c29c
erinh <erin.hazelwood@outlook.com>
2023-02-26 04:59:22 -0500

    workshop three completed

---
+++ src/utils/validateCommentForm.js
@@ -0,0 +1,16 @@
+export const validateCommentForm = (values) => {
+  const errors = {};
+  console.log(values);
+
+  if (!values.rating) {
+    errors.rating = "Required"
+  } 
+
+  if (values.author.length < 2) {
+    errors.author = "Must be at least 2 characters.";
+  } else if (values.author.length > 15) {
+    errors.author = "Must be 15 characters or less";
+  }
+  console.log(errors);
+  return errors;
+};
